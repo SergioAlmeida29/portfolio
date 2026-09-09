a0ee919a59501dc4688e2e8b462e0b59e5bd8272
Sergio Almeida <sergioalmeida29.05@gmail.com>
2026-09-09 07:06:33 +0100

    build: rotatividade de nomes de bundle v2 para invalidar cache

--- vite.config.ts
+++ vite.config.ts
@@ -8,11 +8,14 @@ export default defineConfig({
   build: {
     rollupOptions: {
       output: {
+        entryFileNames: 'assets/[name]-v2-[hash].js',
+        chunkFileNames: 'assets/[name]-v2-[hash].js',
+        assetFileNames: 'assets/[name]-v2-[hash].[ext]',
         manualChunks(id) {
           if (!id.includes('node_modules')) return
 
-          if (id.includes('motion')) return 'motion'
-          if (id.includes('react-dom') || id.includes('/react/')) return 'react'
+          if (id.includes('motion')) return 'motion-vendor'
+          if (id.includes('react-dom') || id.includes('/react/')) return 'react-vendor'
         },
       },
     },
